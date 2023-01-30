deb65b92c709dcf1c1e20025f3b7425ecfecea4f
kidcoconut <iain.mckone@gmail.com>
2023-01-30 11:35:39 -0500

    updated README to satisfy assignment reqs;  updated fastapi module level docs/descr

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # stock-predictor
 a RESTful API with FastAPI and basic time series model to predict stock prices
+
+# Deliverables:
+    * Link to public repository on GitHub
+    * scripts model.py and main.py
+    * requirements.txt (follow instructions for its creation)
+    * README.md that includes how to cURL your API endpoint
+
+# Learning Objectives
+By the end of this lesson, you will be able to:
+    * Develop a RESTful API with FastAPI
+    * Build a basic time series model to predict stock prices
+    * Deploy a FastAPI to AWS EC2
+
+# how to execute, test the app and endpoint
+    * Run the app (locally):
+        uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 49300
+    
+    * Test the endpoint (locally)
+        curl \
+        --header "Content-Type: application/json" \
+        --request POST \
+        --data '{"ticker":"MSFT", "days":7}' \
+        http://0.0.0.0:49300/predict
--- main.py
+++ main.py
@@ -2,7 +2,33 @@ from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 from model import predict, convert
 
-app = FastAPI()
+
+#--- fastAPI self doc descriptors
+kstr_descr = """
+    Fourthbrain Capstone:  MLE10 Cohort
+    
+    Stock Prophet 
+    Getting an initial working system quickly into production
+
+    data -> model -> API -> deployment (Amazon EC2)
+    
+    
+    Deliverables:
+    * Link to your public repository on GitHub
+        * scripts model.py and main.py
+        * requirements.txt (follow instructions for its creation)
+        * README.md that includes how to cURL your API endpoint
+
+    Objectives:
+    * Develop a RESTful API with FastAPI
+    * Build a basic time series model to predict stock prices
+    * Deploy a FastAPI to AWS EC2
+"""
+app = FastAPI(
+    title="MLE10 Week12:  Iain McKone",
+    description=kstr_descr,
+    version="0.0.1"
+)
 
 # pydantic models
 class StockIn(BaseModel):
@@ -12,6 +38,9 @@ class StockIn(BaseModel):
 class StockOut(StockIn):
     forecast: dict
 
+
+
+#--- endpoint for prediction api
 @app.post("/predict", response_model=StockOut, status_code=200)
 def get_prediction(payload: StockIn):
     ticker = payload.ticker
@@ -29,6 +58,14 @@ def get_prediction(payload: StockIn):
     return response_object
 
 
-'''
-uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 49300
+''' For Execution, testing:
+    - Run the app (locally):
+        uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 49300
+    
+    - Test the endpoint (locally)
+        curl \
+        --header "Content-Type: application/json" \
+        --request POST \
+        --data '{"ticker":"MSFT", "days":7}' \
+        http://0.0.0.0:49300/predict
 '''
